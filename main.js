2f7c9a5f9d41f7af62e8f15d4bec493e442200e1
AbhitanSaha <72934030+AbhitanSaha@users.noreply.github.com>
2020-12-01 22:07:57 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -54,9 +54,9 @@ left1();
 console.log("left");
 }
 if(keyPressed=="38"){
-    up2();
-    console.log("up");
-    }
+up2();
+console.log("up");
+}
     if(keyPressed=="40"){
     down2();
     console.log("down");
@@ -71,26 +71,81 @@ if(keyPressed=="38"){
     }
 }
 function up1(){
-
+    if(cary1>0){
+        cary1=cary1-10;
+        console.log("When up is pressed x="+carx1+"y="+cary1);
+        uploadbackground();
+        uploadcar1();
+        uploadcar2();
+    }
 }
 function down1(){
-    
+    if(cary1<500){
+        cary1=cary1+10;
+        console.log("When up is pressed x="+carx1+"y="+cary1);
+        uploadbackground();
+        uploadcar1();
+        uploadcar2();
+    }  
 }
 function right1(){
-    
+    if(carx1<700){
+        carx1=carx1+10;
+        console.log("When up is pressed x="+carx1+"y="+cary1);
+        uploadbackground();
+        uploadcar1();
+        uploadcar2();
+        if(carx1>700){
+            document.getElementById("gamestatus").innerHTML="Car1 Won";
+        }
+    } 
 }
 function left1(){
-    
+    if(carx1<700){
+        carx1=carx1+10;
+        console.log("When up is pressed x="+carx1+"y="+cary1);
+        uploadbackground();
+        uploadcar1();
+        uploadcar2();
+}
 }
 function up2(){
-
+    if(cary2>0){
+        cary2=cary2-10;
+        console.log("When up is pressed x="+carx2+"y="+cary2);
+        uploadbackground();
+        uploadcar2();
+        uploadcar1();
+    }
 }
 function down2(){
-    
+    if(cary2<500){
+        cary2=cary2+10;
+        console.log("When up is pressed x="+carx2+"y="+cary2);
+        uploadbackground();
+        uploadcar2();
+        uploadcar1();
+    } 
 }
 function right2(){
-    
+    if(carx2>0){
+        carx2=carx2+10;
+        console.log("When up is pressed x="+carx2+"y="+cary2);
+        uploadbackground();
+        uploadcar2();
+        uploadcar1();
+        if(carx2>700){
+            document.getElementById("gamestatus").innerHTML="Car2 Won";
+        }
+
+    }
 }
 function left2(){
-    
+    if(carx2<700){
+        carx2=carx2-10;
+        console.log("When up is pressed x="+carx2+"y="+cary2);
+        uploadbackground();
+        uploadcar2();
+        uploadcar1();
+    }
 }
